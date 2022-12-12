d5c34ffc7925a9c1669377820582a7984f62325d
Hexalyse <reversebob@gmail.com>
2022-12-12 11:58:02 +0100

    day 4

---
+++ 04/b.ts
@@ -0,0 +1,20 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const pairs = input.split("\n");
+
+// Each pair looks like this:
+// x-y,a-b
+// where x-y is a range, and a-b another range
+
+// Calculate in how many pairs there is an overlap
+const overlap = pairs.reduce((acc, pair) => {
+  const [range1, range2] = pair.split(",");
+  const [min1, max1] = range1.split("-").map((x) => parseInt(x));
+  const [min2, max2] = range2.split("-").map((x) => parseInt(x));
+  if ((min1 <= max2 && min2 <= max1) || (min2 <= max1 && min1 <= max2)) {
+    acc++;
+  }
+  return acc;
+}, 0);
+
+console.log(overlap);
